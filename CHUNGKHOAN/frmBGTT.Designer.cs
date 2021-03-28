a4aefb6de20a12ede23a7cfef04a235d29ee9a1c
Hoaiphu <hoaiphu.ptit@gmail.com>
2021-03-28 13:56:17 +0700

    Initial commit

---
+++ CHUNGKHOAN/frmBGTT.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace CHUNGKHOAN
+{
+    partial class frmBGTT
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dsCHUNGKHOAN = new CHUNGKHOAN.dsCHUNGKHOAN();
+            this.bdsBANGGIATRUCTUYEN = new System.Windows.Forms.BindingSource(this.components);
+            this.bANGGIATRUCTUYENTableAdapter = new CHUNGKHOAN.dsCHUNGKHOANTableAdapters.BANGGIATRUCTUYENTableAdapter();
+            this.tableAdapterManager = new CHUNGKHOAN.dsCHUNGKHOANTableAdapters.TableAdapterManager();
+            this.lblKq1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dsCHUNGKHOAN)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bdsBANGGIATRUCTUYEN)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dsCHUNGKHOAN
+            // 
+            this.dsCHUNGKHOAN.DataSetName = "dsCHUNGKHOAN";
+            this.dsCHUNGKHOAN.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // bdsBANGGIATRUCTUYEN
+            // 
+            this.bdsBANGGIATRUCTUYEN.DataMember = "BANGGIATRUCTUYEN";
+            this.bdsBANGGIATRUCTUYEN.DataSource = this.dsCHUNGKHOAN;
+            // 
+            // bANGGIATRUCTUYENTableAdapter
+            // 
+            this.bANGGIATRUCTUYENTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.BANGGIATRUCTUYENTableAdapter = this.bANGGIATRUCTUYENTableAdapter;
+            this.tableAdapterManager.LENHDATTableAdapter = null;
+            this.tableAdapterManager.LENHKHOPTableAdapter = null;
+            this.tableAdapterManager.UpdateOrder = CHUNGKHOAN.dsCHUNGKHOANTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // lblKq1
+            // 
+            this.lblKq1.AutoSize = true;
+            this.lblKq1.Location = new System.Drawing.Point(96, 601);
+            this.lblKq1.Name = "lblKq1";
+            this.lblKq1.Size = new System.Drawing.Size(42, 17);
+            this.lblKq1.TabIndex = 2;
+            this.lblKq1.Text = "label1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1266, 546);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // frmBGTT
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1266, 672);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblKq1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "frmBGTT";
+            this.Text = "Bảng giá trực tuyến";
+            this.Load += new System.EventHandler(this.frmBGTT_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dsCHUNGKHOAN)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bdsBANGGIATRUCTUYEN)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private dsCHUNGKHOAN dsCHUNGKHOAN;
+        private System.Windows.Forms.BindingSource bdsBANGGIATRUCTUYEN;
+        private dsCHUNGKHOANTableAdapters.BANGGIATRUCTUYENTableAdapter bANGGIATRUCTUYENTableAdapter;
+        private dsCHUNGKHOANTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.Label lblKq1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ CHUNGKHOAN/frmBGTT.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CHUNGKHOAN
+{
+    public partial class frmBGTT : DevExpress.XtraEditors.XtraForm
+    {
+        private int changeCount = 0;
+        private const string tableName = "BANGGIATRUCTUYEN";
+        private const string statusMessage = "Đã có {0} thay đổi.";
+
+        private SqlConnection connection = null;
+        private SqlCommand command = null/* TODO Change to default(_) if this is not a reference type */;
+        private DataSet dataToWatch = null/* TODO Change to default(_) if this is not a reference type */;
+
+        private bool CanRequestNotifications()
+        {
+            // In order to use the callback feature of the
+            // SqlDependency, the application must have
+            // the SqlClientPermission permission.
+            try
+            {
+                SqlClientPermission perm = new SqlClientPermission(PermissionState.Unrestricted);
+
+                perm.Demand();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public frmBGTT()
+        {
+            InitializeComponent();
+            SqlDependency.Start(GetConnectionString());
+        }
+
+        private string GetConnectionString()
+        {
+            // To avoid storing the connection string in your code,
+            // you can retrive it from a configuration file.
+            // Return "Data Source=THU-PC\TINTIN;Initial Catalog=QLVT;Persist Security Info=True;User ID=sa;Password=kc;Pooling = false"
+            return "Data Source=HOAIPHU-PC;Initial Catalog=CHUNGKHOAN;User ID=sa;Password=123";
+        }
+
+        private string GetSQL()
+        {
+            return "select MACP as [Mã CP],DM_GIA3 as [       Giá 3],DM_KL3 as [KL 3]," +
+                "DM_GIA2 as [       Giá 2],DM_KL2 as [KL 2]," +
+                "DM_GIA1 as [       Giá 1],DM_KL1 as [KL 1]," +
+                "KL_GIA as [       Giá],KL_KL as [KL]," +
+                "DB_GIA1 as [       Giá 1],DB_KL1 as [KL 1]," +
+                "DB_GIA2 as [       Giá 2],DB_KL2 as [KL 2]," +
+                "DB_GIA3 as [       Giá 3],DB_KL3 as [KL 3] from dbo.BANGGIATRUCTUYEN";
+        }
+
+        private void GetData()
+        {
+            // Empty the dataset so that there is only
+            // one batch worth of data displayed.
+            dataToWatch.Clear();
+
+            // Make sure the command object does not already have
+            // a notification object associated with it.
+
+            command.Notification = null;
+
+            // Create and bind the SqlDependency object
+            // to the command object.
+
+            SqlDependency dependency = new SqlDependency(command);
+            dependency.OnChange += dependency_OnChange;
+
+            using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+                adapter.Fill(dataToWatch, tableName);
+
+                this.dataGridView1.DataSource = dataToWatch;
+                this.dataGridView1.DataMember = tableName;
+            }
+        }
+
+        private void dependency_OnChange(object sender, SqlNotificationEventArgs e)
+        {
+
+            // This event will occur on a thread pool thread.
+            // It is illegal to update the UI from a worker thread
+            // The following code checks to see if it is safe update the UI.
+            ISynchronizeInvoke i = (ISynchronizeInvoke)this;
+
+            // If InvokeRequired returns True, the code is executing on a worker thread.
+            if (i.InvokeRequired)
+            {
+                // Create a delegate to perform the thread switch
+                OnChangeEventHandler tempDelegate = new OnChangeEventHandler(dependency_OnChange);
+
+                object[] args = new[] { sender, e };
+
+                // Marshal the data from the worker thread
+                // to the UI thread.
+                i.BeginInvoke(tempDelegate, args);
+
+                return;
+            }
+
+            // Remove the handler since it's only good
+            // for a single notification
+            SqlDependency dependency = (SqlDependency)sender;
+
+            dependency.OnChange -= dependency_OnChange;
+
+            changeCount += 1;
+            this.lblKq1.Text = string.Format(statusMessage, changeCount);
+
+            // At this point, the code is executing on the
+            // UI thread, so it is safe to update the UI.
+
+
+            // Reload the dataset that's bound to the grid.
+            GetData();
+        }
+
+        private void BatDau()
+        {
+            changeCount = 0;
+            // Remove any existing dependency connection, then create a new one.
+            SqlDependency.Stop(GetConnectionString());
+            try
+            {
+                SqlDependency.Start(GetConnectionString());
+            }
+            catch (Exception ex)
+            {
+                /*Interaction.MsgBox(ex.Message, MsgBoxStyle.Exclamation);
+                return;*/
+            }
+            if (connection == null)
+            {
+                connection = new SqlConnection(GetConnectionString());
+                connection.Open();
+            }
+            if (command == null)
+                // GetSQL is a local procedure that returns
+                // a paramaterized SQL string. You might want
+                // to use a stored procedure in your application.
+                command = new SqlCommand(GetSQL(), connection);
+
+            if (dataToWatch == null)
+                dataToWatch = new DataSet();
+            GetData();
+        }
+
+        private void bANGGIATRUCTUYENBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.bdsBANGGIATRUCTUYEN.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.dsCHUNGKHOAN);
+
+        }
+
+        private void frmBGTT_Load(object sender, EventArgs e)
+        {
+
+            // TODO: This line of code loads data into the 'dsCHUNGKHOAN.BANGGIATRUCTUYEN' table. You can move, or remove it, as needed.
+            if (CanRequestNotifications() == true)
+                BatDau();
+            else
+                MessageBox.Show("Bạn chưa kích hoạt dịch vụ Broker", "ngu", MessageBoxButtons.OK);
+
+        }
+
+        private void frmBGTT_FormClosed(System.Object sender, System.Windows.Forms.FormClosedEventArgs e)
+        {
+            SqlDependency.Stop(GetConnectionString());
+            if (connection != null)
+                connection.Close();
+        }
+    }
+}
